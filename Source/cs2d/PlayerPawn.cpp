a9f86d4558ab23fcdc7b1a08f6b340c7079f9e83
Simone Meneghin <simone.meneghin.98@outlook.it>
2020-02-29 09:58:57 +0100

    Add player staticmesh

--- Source/cs2d/PlayerPawn.cpp
+++ Source/cs2d/PlayerPawn.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PlayerPawn.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/StaticMeshComponent.h"
@@ -10,7 +9,7 @@
 // Sets default values
 APlayerPawn::APlayerPawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
@@ -19,18 +18,21 @@ APlayerPawn::APlayerPawn()
 	Player = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Collider"));
 	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
 	Weapon = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Weapon"));
-	UCameraComponent* Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	UCameraComponent *Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 
 	Body->SetupAttachment(Player);
 
-	static ConstructorHelpers::FObjectFinder<UStaticMesh>MeshAsset(TEXT("Content/StarterContent/Shapes/Shape_NarrowCapsule.uasset"));
-	UStaticMesh* Asset = MeshAsset.Object;
-	Body->SetStaticMesh(Asset);
+	ConstructorHelpers::FObjectFinder<UStaticMesh> MeshAsset(TEXT("/Game/StarterContent/Shapes/Shape_NarrowCapsule.Shape_NarrowCapsule"));
+
+	if (MeshAsset.Succeeded())
+	{
+		Body->SetStaticMesh(MeshAsset.Object);
+	}
 
 	Weapon->SetRelativeScale3D(FVector(0.75f, 0.1f, 0.1f));
-	Weapon->SetRelativeLocation(FVector(15.0f,15.0f,50.0f));
+	Weapon->SetRelativeLocation(FVector(15.0f, 15.0f, 50.0f));
 	Weapon->SetupAttachment(Body);
-	Camera->SetRelativeLocation(FVector(0.0f,0.0f,500.0f));
+	Camera->SetRelativeLocation(FVector(0.0f, 0.0f, 500.0f));
 	Camera->SetRelativeRotation(FRotator(-90.0f, 0.0f, 0.0f));
 	Camera->SetupAttachment(Player);
 	Player->SetupAttachment(RootComponent);
@@ -40,20 +42,16 @@ APlayerPawn::APlayerPawn()
 void APlayerPawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void APlayerPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
-void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
-
